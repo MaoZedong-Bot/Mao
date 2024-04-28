5c3c0a395de559abc322c32ea57344c8d82787e4
luna <zephyr1112@protonmail.com>
2024-04-28 17:43:05 -0400

    fix status

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const rest = new REST({ version: "10" }).setToken(process.env.DISCORD_TOKEN);
 
 client.on("ready", () => {
   console.log(`Logged in as ${client.user.tag}!`);
-  client.user.setPresence({ activities: [{ name: 'in Huaweicord' }] });
+  client.user.setPresence({ activities: [{ name: 'Fuck Kim Jong-Un Simulator' }] });
 });
 
 client.login(process.env.DISCORD_TOKEN);
