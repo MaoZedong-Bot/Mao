74a37da29e1b5a4319a5fd4d6418684f795b5e70
luna <zephyr1112@protonmail.com>
2024-04-28 17:45:33 -0400

    dont mess with me again… 😈 baka… 😈

--- index.js
+++ index.js
@@ -7,6 +7,14 @@ const rest = new REST({ version: "10" }).setToken(process.env.DISCORD_TOKEN);
 client.on("ready", () => {
   console.log(`Logged in as ${client.user.tag}!`);
   client.user.setPresence({ activities: [{ name: 'Fuck Kim Jong-Un Simulator' }] });
+
+  const channel = client.channels.cache.get('1231228286148018321');
+  if (channel) {
+      channel.send('dont mess with me again… 😈 baka… 😈');
+      channel.send('Mao Zedong v0.0.69 is running');
+  } else {
+      console.error('Could not find the specified channel.');
+  }
 });
 
 client.login(process.env.DISCORD_TOKEN);
