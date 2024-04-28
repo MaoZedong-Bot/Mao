abb2d5b36e3fbf2c14554b37d8bae1651fc59888
luna <zephyr1112@protonmail.com>
2024-04-28 17:39:56 -0400

    index.js template

---
+++ index.js
@@ -0,0 +1,12 @@
+const { REST } = require("@discordjs/rest");
+const Discord = require("discord.js");
+const client = new Discord.Client({ intents: 32767 });
+
+const rest = new REST({ version: "10" }).setToken(process.env.DISCORD_TOKEN);
+
+client.on("ready", () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+  client.user.setPresence({ activities: [{ name: 'in Huaweicord' }] });
+});
+
+client.login(process.env.DISCORD_TOKEN);
