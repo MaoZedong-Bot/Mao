d4c52002d5dc995531ad22da5d18602d2ff9aeed
luna <zephyr1112@protonmail.com>
2024-05-01 11:19:02 -0400

    add weather command, wonky

---
+++ commands/general/weather.js
@@ -0,0 +1,30 @@
+const { SlashCommandBuilder } = require('discord.js');
+const axios = require('axios');
+const { maptiler_key } = require('../../config.json');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('weather')
+		.setDescription('Find weather at a location')
+        .addStringOption(option =>
+            option.setName('location')
+                .setDescription('Place')
+                .setRequired(true)
+        ),
+	async execute(interaction) {
+        const input = interaction.options.getString('location');
+
+        // geocoding
+        const locJson = await axios.get(`https://api.maptiler.com/geocoding/${input}.json?key=${maptiler_key}&types=country,region,subregion,county,municipality&limit=1`);
+        const locJson_parsed = locJson.data;
+        let location = "";
+        locJson_parsed.features.forEach(feature => {
+            location = feature.place_name;
+            console.log(location);
+        })
+
+        await axios.get(`https://wttr.in/${location}?Td&format=%l:+%c%t;+Wind+%w`)
+            .then((res) => interaction.reply(String(res.data)))
+            .catch((err) => interaction.reply(String(err)));
+	},
+};
