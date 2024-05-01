183b58e0c590e50f5889345f6ca9d972b12b3dea
CubeCat <61134618+cubecatdoesthings@users.noreply.github.com>
2024-05-02 01:21:05 +0300

    added user info and github repo lookup commands

---
+++ commands/general/github.js
@@ -0,0 +1,61 @@
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
+const axios = require('axios');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('repo')
+        .setDescription('Get information about a GitHub repository from its link or name')
+        .addStringOption(option =>
+            option.setName('repository')
+                .setDescription('The GitHub repository link or name')
+                .setRequired(true)),
+    async execute(interaction) {
+        const repository = interaction.options.getString('repository');
+        let owner, repoName;
+
+        if (repository.startsWith('http')) {
+            const repoRegex = /github\.com\/([^/]+)\/([^/]+)(?:\/)?$/i;
+            const match = repository.match(repoRegex);
+
+            if (!match) {
+                return interaction.reply('Invalid GitHub repository link.');
+            }
+
+            owner = match[1];
+            repoName = match[2];
+        } else {
+            const parts = repository.split('/');
+            if (parts.length !== 2) {
+                return interaction.reply('Invalid GitHub repository name.');
+            }
+            owner = parts[0];
+            repoName = parts[1];
+        }
+
+        const apiUrl = `https://api.github.com/repos/${owner}/${repoName}`;
+
+        try {
+            const response = await axios.get(apiUrl);
+            const repoData = response.data;
+
+            const embed = new EmbedBuilder()
+                .setColor(0x0099ff)
+                .setTitle(repoData.full_name)
+                .setURL(repoData.html_url)
+                .setDescription(repoData.description || 'No description provided.')
+                .addFields(
+                    { name: 'Stars', value: repoData.stargazers_count.toLocaleString(), inline: true },
+                    { name: 'Forks', value: repoData.forks_count.toLocaleString(), inline: true },
+                    { name: 'Language', value: repoData.language || 'Not specified', inline: true },
+                    { name: 'Owner', value: repoData.owner.login, inline: true },
+                    { name: 'Created At', value: new Date(repoData.created_at).toUTCString(), inline: true },
+                )
+                .setTimestamp();
+
+            interaction.reply({ embeds: [embed] });
+        } catch (error) {
+            console.error('Error fetching repository data:', error);
+            interaction.reply('An error occurred while fetching repository data.');
+        }
+    },
+};
---
+++ commands/general/userinfo.js
@@ -0,0 +1,29 @@
+const { SlashCommandBuilder } = require('discord.js');
+const { EmbedBuilder } = require('discord.js');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('userinfo')
+        .setDescription('Get information about a user')
+        .addUserOption(option =>
+            option.setName('user')
+                .setDescription('The user to get information about')
+                .setRequired(false)),
+    async execute(interaction) {
+        const user = interaction.options.getUser('user') || interaction.user;
+
+        const embed = new EmbedBuilder()
+            .setColor(0x0099ff)
+            .setTitle('User Information')
+            .setThumbnail(user.displayAvatarURL({ dynamic: true }))
+            .addFields(
+                { name: 'User ID', value: user.id, inline: true },
+                { name: 'Username', value: user.username, inline: true },
+                { name: 'Bot', value: user.bot ? 'Yes' : 'No', inline: true },
+                { name: 'Created At', value: user.createdAt.toUTCString() },
+            )
+            .setTimestamp()
+
+        interaction.reply({ embeds: [embed] });
+    },
+};
