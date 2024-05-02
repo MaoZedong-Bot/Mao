8476e5822e30e4922e3dce4b032a52dca454aac3
CubeCat <61134618+cubecatdoesthings@users.noreply.github.com>
2024-05-02 15:30:22 +0300

    holy shit

---
+++ commands/android/xfu.js
@@ -0,0 +1,45 @@
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
+const { getROMVersions } = require('../mod/helper/romLookup');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('miui')
+        .setDescription('Get MIUI or HyperOS versions for a device using codename as input')
+        .addStringOption(option =>
+            option.setName('device')
+                .setDescription('Device codename')
+                .setRequired(true))
+        .addStringOption(option =>
+            option.setName('rom_type')
+                .setDescription('Choose ROM type')
+                .setRequired(true)
+                .addChoices(
+                    { name: 'Fastboot', value: 'fastboot' },
+                    { name: 'Recovery', value: 'recovery' },
+                )),
+    async execute(interaction) {
+        try {
+            const device = interaction.options.getString('device');
+            const romType = interaction.options.getString('rom_type');
+
+            const romVersions = await getROMVersions(device, romType);
+
+            const romTypeTitle = romType.charAt(0).toUpperCase() + romType.slice(1);
+            const embed = new EmbedBuilder()
+                .setColor(0x0099FF)
+                .setTitle(`${romTypeTitle} ROM Versions for ${device}`)
+                .addFields(
+                    romVersions.map(({ deviceName, branch, romName }) => ({
+                        name: `${deviceName} ${branch}`,
+                        value: romName
+                    }))
+                )
+                .setTimestamp();
+
+            interaction.reply({ embeds: [embed] });
+        } catch (error) {
+            console.error('Error executing command:', error);
+            interaction.reply('An error occurred while processing your request.');
+        }
+    },
+};
