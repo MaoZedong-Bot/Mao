817f52f25b77a8ee0e0eb85a9b039a7d0cdf3ecf
UsrBinLuna <zephyr1112@protonmail.com>
2024-12-24 15:21:08 -0300

    about command improvements

--- commands/general/about.js
+++ commands/general/about.js
@@ -22,7 +22,7 @@ module.exports = {
             .addFields(
                 {
                     name: 'Version',
-                    value: `${version}`,
+                    value: `\`${version}\``,
                     inline: true
                 },
                 {
