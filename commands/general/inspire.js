8b58237b98ca6133c0a9d34ef7b64ca20f105ffc
Luna <67978777+UsrBinLuna@users.noreply.github.com>
2025-03-19 22:10:53 -0300

    new ai 2

--- commands/general/inspire.js
+++ commands/general/inspire.js
@@ -16,7 +16,7 @@ async execute(interaction) {
         messages: [
         {
             role: 'user',
-            content: 'generate a single completely absurd made up inspirational quote  about slop and the internet or just anything stupid',
+            content: 'Generate a completely brain-meltingly stupid inspirational quote about slop, the internet, or literally anything that makes zero sense. Make it sound deep, but upon closer inspection, it should have the intellectual value of a wet sock in a blender. I want it to feel like it was written by a sentient potato having an existential crisis.',
         },
         ],
 });
